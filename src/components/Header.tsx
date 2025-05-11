8b6be950b23e42b7f512d07b12382fd260b8d56e
Vicent Sanjaime <vsanjaime@cartodb.com>
2025-05-11 21:39:00 +0200

    header mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,31 @@
 import logo from '../assets/images/logo.png';
 import './Header.css';
 export function Header() {
+
+    const title = "Decasaalcole";
+    const subtitle = "Herramienta de cálculo de tiempos de viaje desde tu casa, a todos los centros educativos de la Comunitat Valenciana";
+    
     return (
         <>
-        <div className="header">
-            <div className="header_logo">
-                <img src={logo} alt="Decasaalcole logo" width="80" height="80" />
+        {window.innerWidth < 700 ? (
+            <div className="header">
+                <div className="header_top">
+                    <div className="header_logo">
+                        <img src={logo} alt="Decasaalcole logo" width="50" height="50" />
+                    </div>
+                    <h1>{title}</h1>
+                </div>
+                <h3>{ subtitle }</h3>
             </div>
-            <h1>Decasaalcole</h1>
-            <h3>Herramienta de cálculo de tiempos de viaje desde tu casa, a todos los centros educativos de la Comunitat Valenciana</h3>
-        </div>
+        ) : (
+            <div className="header">
+                <div className="header_logo">
+                    <img src={logo} alt="Decasaalcole logo" width="80" height="80" />
+                </div>
+                <h1>{ title }</h1>
+                <h3>{ subtitle }</h3>
+            </div>
+        )}
         </>
     )
 }
