6843046d54f4356d3b5a6b2be2796d94f5aaca7f
Vicent Sanjaime <vsanjaime@cartodb.com>
2025-05-15 17:19:33 +0200

    footer and header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,21 @@
 import logo from '../assets/images/logo.png';
 import './Footer.css';
+
 export function Footer() {
+    const appName = "Decasaalcole";
+    
     return (
-        <>
-        <div className="footer">
+        <footer className="footer">
             <div className="footer_body">
-                <img src={logo} alt="Decasaalcole logo" width="30" height="30" />
-                <p>Decasaalcole</p>
+                <img 
+                    src={logo} 
+                    alt={`${appName} logo`}
+                    width={30} 
+                    height={30}
+                />
+                <p>{appName}</p>
             </div>
-        </div>
-        </>
-    )
+        </footer>
+    );
 }
   
